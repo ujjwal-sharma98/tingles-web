595b8f39289b1f08cd583272574ceda95fe2c7a8
ujjwal <ujjwal.develop@gmail.com>
2025-03-21 00:09:05 +0530

    build

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("@mui")) return "mui"; // Separate MUI into its own chunk
+            if (id.includes("react")) return "react"; // Separate React-related code
+            return "vendor"; // Other external libraries
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 700, // Prevent warnings (optional)
+  },
 })
