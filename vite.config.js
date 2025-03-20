e5b4bd858092611a40c9db971d79ea1a1b2289aa
ujjwal <ujjwal.develop@gmail.com>
2025-03-20 21:52:41 +0530

    build

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    minify: "esbuild", // ✅ Uses esbuild instead of Terser
-  },
 })
